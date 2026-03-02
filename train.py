68a1d5aff26eff649633ea3ed512b266897476f6
SergKurchev <sergkurchev@gmail.com>
2026-03-02 13:01:24 +0300

    trajectory now consistent with notation

--- train.py
+++ train.py
@@ -125,7 +125,8 @@ def main():
                     else:
                         next_state, reward, terminal_state, truncated, info = env.step_manual(action)
                     
-                    experience = (state, action, reward, next_state, terminal_state)
+                    # Формируем опыт только из нужных элементов (без next_state и terminal_state)
+                    experience = (state, action, reward)
                     trajectory.append(experience)
                     
                     state = next_state
